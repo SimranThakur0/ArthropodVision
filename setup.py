110a76134f0cdd5214279fd2a200a949715a501a
Simran thakur <shivangithakur7300@gmail.com>
2024-12-18 18:12:16 +0530

    initial commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
   name = "ObjMap",
   version = "1.0",
-  author = "Shrish",
-  author_email = "shrishkamboz@gmail.com",
+  author = "Simran Thakur",
+  author_email = "shivangithakur7300@gmail.com",
   description = "An advanced object detection system powered by YOLOv8, designed for real-time and efficient identification of objects in images and videos. Leveraging state-of-the-art deep learning techniques.",
   packages=find_packages(),
 )
